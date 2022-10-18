ba0372cd73b496fcaf60077233cadfeeeda7df4b
Bruce <jacobbruce880@gmail.com>
2022-10-18 01:21:43 -0700

    Matches a single character in the middle of a three-word string

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#Matches 0 or more occurrence of a character
+puts ARGV[0].scan(/^hbt*n/).join
---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#Matches a single character in the middle of a string
+puts ARGV[0].scan(/^h.{1}n/).join
